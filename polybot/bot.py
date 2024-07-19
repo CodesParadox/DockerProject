8a1307b1c38fe39f753202f44b8251c6733a40fa
CodesParadox <galbraymok@gmail.com>
2024-07-19 09:25:20 -0700

    finished ^.^

--- polybot/bot.py
+++ polybot/bot.py
@@ -3,65 +3,40 @@ from loguru import logger
 import os
 import time
 from telebot.types import InputFile
-import boto3
-import requests
-import json
 from img_proc import Img
-from pathlib import Path
-import yaml
+from collections import Counter
+import json
+
 
 class Bot:
-    """
-    The Bot class is a simple echo bot that communicates with Telegram servers.
-    """
 
     def __init__(self, token, telegram_chat_url):
-        """
-        Initializes the Bot with a token and a Telegram chat URL.
-
-        :param token: The Telegram bot token.
-        :param telegram_chat_url: The URL of the Telegram chat.
-        """
+        self.image_path = ""
+        self.images = []
+        # create a new instance of the TeleBot class.
+        # all communication with Telegram servers are done using self.telegram_bot_client
         self.telegram_bot_client = telebot.TeleBot(token)
+        # remove any existing webhooks configured in Telegram servers
         self.telegram_bot_client.remove_webhook()
         time.sleep(0.5)
+        # set the webhook URL
         self.telegram_bot_client.set_webhook(url=f'{telegram_chat_url}/{token}/', timeout=60)
         logger.info(f'Telegram Bot information\n\n{self.telegram_bot_client.get_me()}')
 
     def send_text(self, chat_id, text):
-        """
-        Sends a text message to a chat.
-
-        :param chat_id: The ID of the chat to send the message to.
-        :param text: The text of the message to send.
-        """
         self.telegram_bot_client.send_message(chat_id, text)
 
     def send_text_with_quote(self, chat_id, text, quoted_msg_id):
-        """
-        Sends a text message to a chat, quoting another message.
-
-        :param chat_id: The ID of the chat to send the message to.
-        :param text: The text of the message to send.
-        :param quoted_msg_id: The ID of the message to quote.
-        """
         self.telegram_bot_client.send_message(chat_id, text, reply_to_message_id=quoted_msg_id)
 
-    def is_current_msg_photo(self, msg):
-        """
-        Checks if the current message is a photo.
-
-        :param msg: The message to check.
-        :return: True if the message is a photo, False otherwise.
-        """
+    @staticmethod
+    def is_current_msg_photo(msg):
         return 'photo' in msg
 
     def download_user_photo(self, msg):
         """
-        Downloads the photo that was sent to the Bot to `photos` directory.
-
-        :param msg: The message containing the photo.
-        :return: The path of the downloaded photo.
+        Downloads the photos that sent to the Bot to `photos` directory (should be existed)
+        :return:
         """
         if not self.is_current_msg_photo(msg):
             raise RuntimeError(f'Message content of type \'photo\' expected')
@@ -78,98 +53,193 @@ class Bot:
 
         return file_info.file_path
 
-    def send_photo(self, chat_id, img_path):
-        """
-        Sends a photo to a chat.
-
-        :param chat_id: The ID of the chat to send the photo to.
-        :param img_path: The path of the photo to send.
-        """
-        if not os.path.exists(img_path):
+    def send_photo(self, chat_id, image_path, caption=None):
+        if not os.path.exists(image_path):
             raise RuntimeError("Image path doesn't exist")
 
-        self.telegram_bot_client.send_photo(chat_id, InputFile(img_path))
-
-    def handle_message(self, msg):
-        """
-        Handles incoming messages. This method should be overridden by subclasses.
-
-        :param msg: The incoming message.
-        """
-        logger.info(f'Incoming message: {msg}')
-        self.send_text(msg['chat']['id'], f'Your original message: {msg["text"]}')
-
-
-class QuoteBot(Bot):
-    """
-    The QuoteBot class extends the Bot class to quote incoming messages.
-    """
-
-    def handle_message(self, msg):
-        """
-        Handles incoming messages by quoting them.
-
-        :param msg: The incoming message.
-        """
-        logger.info(f'Incoming message: {msg}')
-
-        if msg["text"] != 'Please don\'t quote me':
-            self.send_text_with_quote(msg['chat']['id'], msg["text"], quoted_msg_id=msg["message_id"])
-
-
-class ImageProcessingBot(Bot):
-    """
-    The ImageProcessingBot class extends the Bot class to process incoming photo messages.
-    """
-
-    def __init__(self, token, telegram_chat_url):
-        super().__init__(token, telegram_chat_url)
-        self.config = yaml.safe_load(open('config.yaml'))
-        self.yolo5_url = self.config['yolo5_url']
-
-    def handle_message(self, msg):
-        """
-        Handles incoming photo messages by processing them and sending them back to the user.
-
-        :param msg: The incoming message.
-        """
-        logger.info(f'Incoming message: {msg}')
-
-        if self.is_current_msg_photo(msg):
-            img_path = self.download_user_photo(msg)
-            img = Img(img_path)
-            img.rotate()
-            img.salt_n_pepper()
-            img_path = 'rotated_' + img_path
-            img.save(img_path)
-            self.send_photo(msg['chat']['id'], img_path)
+        if caption is None:
+            self.telegram_bot_client.send_photo(
+                chat_id,
+                InputFile(image_path)
+            )
         else:
-            self.send_text(msg['chat']['id'], 'Please send a photo')
-
-        # save the filtered image to a new file
-        filtered_img_path = img.save_img()
-        # send the filtered image to the user
-        self.send_photo(msg['chat']['id'], filtered_img_path)
-
-
-class ObjectDetectionBot(Bot):
-    def __init__(self, token, telegram_chat_url):
-        super().__init__(token, telegram_chat_url)
-        self.config = yaml.safe_load(open('config.yaml'))
-        self.yolo5_url = self.config['yolo5_url']
+            self.telegram_bot_client.send_photo(
+                chat_id,
+                InputFile(image_path),
+                caption=caption
+            )
 
     def handle_message(self, msg):
+        """
+        Determines what to do based on the message received from the user:
+        Ignore files without a specific processing function.
+        Reply to any text message with a qute of the message and sending user name.
+        If the message has a photo in it - determines whether there's 1 or 2 images in the message and calls the menu function accordingly.
+        :return:
+        """
         logger.info(f'Incoming message: {msg}')
+        chat_id = msg['chat']['id']
+        try:
+            if 'text' in msg:
+                if 'entities' in msg and msg['entities'][0]['type'] == 'bot_command':
+                    self.handle_filter_command(msg)
+                elif msg["text"] != 'Please don\'t quote me':
+                    self.send_text_with_quote(
+                        chat_id,
+                        f"Hey! <('-'<)<('.')>(>'-')> \n {msg['text']}",
+                        quoted_msg_id=msg['message_id']
+                    )
+                    # self.send_text_with_quote(msg['chat']['id'], msg[f"Hey! <('-'<)<('.')>(>'-')> \n {msg}"], quoted_msg_id=msg["message_id"])
+                else:
+                    self.send_text(chat_id, "Hey :D")
+            elif 'document' in msg:
+                pass
+            elif 'photo' in msg:
+                chat_id = msg['chat']['id']
+                if 'media_group_id' in msg:
+                    self.image_path = self.download_user_photo(msg)
+                    self.images.append(self.image_path)
+                    if len(self.images) == 2:
+                        self.send_photo_command_menu(chat_id)
+                else:
+                    self.image_path = self.download_user_photo(msg)
+                    self.send_photo_command_menu(chat_id)
+            elif 'audi' in msg:
+                pass
+            elif 'voice' in msg:
+                pass
+            else:
+                pass
+        except Exception:
+            self.send_text(chat_id, "Please try again")
 
-        if self.is_current_msg_photo(msg):
-            photo_path = self.download_user_photo(msg)
+    # def send_text_with_quote(self, chat_id, text, quoted_msg_id):
+    #     try:
+    #         self.telegram_bot_client.send_message(chat_id, text, reply_to_message_id=quoted_msg_id)
+    #     except telebot.apihelper.ApiTelegramException as e:
+    #         logger.error(f"Error sending text with quote: {e}")
+    #         self.send_text(chat_id, text)  # Send the text without quote
 
-            # Upload the photo to S3
-            s3 = boto3.client('s3')
-            s3.upload_file(photo_path, 'braymok-s3-docker', photo_path)
-            # Send an HTTP request to the `yolo5` service for prediction
-            response = requests.get(f'{self.yolo5_url}/predict?imgName={photo_path}')
-            # Send the returned results to the Telegram end-user
-            self.send_text(msg['chat']['id'], response.text)
-        else:
-            self.send_text(msg['chat']['id'], 'Please send a photo')
+    def send_photo_command_menu(self, chat_id):
+        """
+        Sends a links menu of the available filters as a response message to any image or images group received from the user
+        :return:
+        """
+        command_menu = "Please reply with the desired filter command:\n"
+        command_menu += "/blur - Apply blur filter\n"
+        command_menu += "/contour - Apply contour filter\n"
+        command_menu += "/rotate - Rotate the image\n"
+        command_menu += "/salt_n_pepper - Apply salt and pepper noise\n"
+        command_menu += "/concat - Requires 2 Images to be uploaded, & collages them together\n"
+        command_menu += "/segment - Segment the image\n"
+        command_menu += "/predict - Identify the image content using YOLO5\n"
+        self.send_text(chat_id, command_menu)
+
+    def send_photo_command_submenu(self, chat_id):
+        """
+        Sends a links submenu of directions options when the user clicks the concat filter option
+        :return:
+        """
+        command_submenu = "Please reply with the desired direction:\n"
+        command_submenu += "/horizontal\n"
+        command_submenu += "/vertical\n"
+        self.send_text(chat_id, command_submenu)
+
+    def handle_filter_command(self, msg):
+        """
+        Handles the user's choice of a filter by calling the corresponding function with a filter command
+        :return:
+        """
+        chat_id = msg['chat']['id']
+        command = msg['text'].split()[0]
+        error_found = False
+        processed_image_path = ""
+
+        if command == '/concat':
+            self.send_photo_command_submenu(chat_id)
+        elif command == '/horizontal' or command == '/vertical':
+            if self.images:
+                img1 = Img(self.images[0])
+                img2 = Img(self.images[1])
+                result = img1.concat(img2, command)
+                if result[1] == 500:
+                    self.send_text(chat_id, result[0])
+                    self.image_path = ""
+                    self.images = []
+                    return
+                processed_image_path = img1.save_img()
+            else:
+                error_found = True
+        elif self.image_path:
+            img = Img(self.image_path)
+            if command == '/blur':
+                img.blur()
+            elif command == '/contour':
+                img.contour()
+            elif command == '/rotate':
+                img.rotate()
+            elif command == '/salt_n_pepper':
+                img.salt_n_pepper()
+            elif command == '/segment':
+                img.segment()
+            elif command == '/predict':
+                try:
+                    yolo_service_url = os.environ['YOLO_SERVICE_URL']
+                    image_path = os.path.abspath(self.image_path)
+                    image_name = os.path.basename(self.image_path)
+                    prediction_summary = img.upload_and_predict(yolo_service_url, image_path, image_name)
+                    caption = self.prediction_decode(prediction_summary)
+                    self.send_photo(chat_id, image_path, caption)
+                    self.images = []
+                    self.image_path = ""
+                    return
+                except Exception as e:
+                    logger.error(f"Error during prediction: {e}")
+                    self.send_text(chat_id, "Error during prediction. Please try again.")
+                    return
+            else:
+                # Invalid filter command
+                self.send_text(chat_id, "Invalid filter command. Please choose from the command menu.")
+                return
+            processed_image_path = img.save_img()
+        if error_found is False and processed_image_path:
+            self.send_photo(chat_id, processed_image_path)
+            self.image_path = ""
+            self.images = []
+
+    @staticmethod
+    def prediction_decode(prediction_summary):
+        try:
+            # Check the type of prediction_summary and log it
+            logger.info(f"Prediction summary type: {type(prediction_summary)}")
+            logger.info(f"Prediction summary content: {prediction_summary}")
+
+            # Assuming prediction_summary is a list of dictionaries
+            if isinstance(prediction_summary, dict) and 'result_path' in prediction_summary:
+                result_path = prediction_summary['result_path']
+                if 'labels' in result_path:
+                    labels = result_path['labels']
+                    classes = [label['class'] for label in labels]
+                else:
+                    raise KeyError("Missing 'labels' in 'result_path'")
+            else:
+                raise KeyError("Invalid structure for prediction_summary")
+
+            quantities = Counter(classes)
+            response = "Prediction Summary:\n"
+            response += "\n".join([f"{key.capitalize()} - {value}" for key, value in quantities.items()])
+            return response
+
+        except (json.JSONDecodeError, KeyError, TypeError) as e:
+            logger.error(f'Error decoding prediction summary: {e}')
+            return "Error decoding prediction summary"
+
+        #     labels = prediction_summary['labels']
+        #     classes = [label['class'] for label in labels]
+        #     quantities = Counter(classes)
+        #     response = "Prediction Summary:\n"
+        #     response += "\n".join([f"{key.capitalize()} - {value}\n" for key, value in quantities.items()])
+        #     return response
+        # except (json.JSONDecodeError, KeyError) as e:
+        #     logger.error(f'Error decoding JSON response: {e}')
+        #     return "Error decoding prediction summary"
--- yolo5/app.py
+++ yolo5/app.py
@@ -1,105 +1,170 @@
 import time
 from pathlib import Path
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from detect import run
 import uuid
 import yaml
-import boto3
 from loguru import logger
-import os
-import requests
 from pymongo import MongoClient
+import os
+import boto3
+from botocore.exceptions import ClientError
 
+logger = logger.opt(colors=True)
 
+# Environment variables
+images_bucket = os.getenv('BUCKET_NAME')
 
-images_bucket = os.environ['BUCKET_NAME']
+mongo_client = MongoClient(os.environ['MONGO_URI'])
+db = mongo_client['predictions_db']
+collection = db['predictions']
 
 with open("data/coco128.yaml", "r") as stream:
     names = yaml.safe_load(stream)['names']
 
+# Initialize Flask
 app = Flask(__name__)
-## Initialize S3 and mongoDB clients
-s3 = boto3.client('s3')
-client = MongoClient("mongodb://localhost:27017/")
-db = client["predictions"]
-collection = db["prediction_summary"]
 
+
+def download_from_s3(bucket_name, s3_key, local_path):
+    s3_client = boto3.client('s3')
+
+    try:
+        local_path = Path(local_path)
+        if not local_path.exists():
+            local_path.parent.mkdir(parents=True, exist_ok=True)
+        local_file_path = str(local_path.resolve())
+        s3_client.download_file(bucket_name, s3_key, local_file_path)
+        logger.info(f'<green>Successfully downloaded {s3_key} from {bucket_name}</green>')
+    except ClientError as e:
+        logger.error(f'<red>Error downloading from S3: {e}</red>')
+        raise
+
+
+def upload_to_s3(local_path, s3_key):
+    bucket_name = os.getenv('BUCKET_NAME')
+    s3_client = boto3.client('s3')
+
+    try:
+        s3_client.upload_file(local_path, bucket_name, s3_key)
+        image_url = f"https://{bucket_name}.s3.amazonaws.com/{s3_key}"
+        logger.info(f'<green>Successfully uploaded {local_path} to s3://{bucket_name}/{s3_key}</green>')
+        return image_url
+    except ClientError as e:
+        logger.error(f"Error uploading file to S3: {e}")
+        raise
+
+    # try:
+    #     s3_client.upload_file(local_path, bucket_name, s3_key)
+    #     logger.info(f'<green>Successfully uploaded {local_path} to s3://{bucket_name}/{s3_key}</green>')
+    # except ClientError as e:
+    #     logger.error(f'<red>Error uploading to S3: {e}</red>')
+    #     raise
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Generates a UUID for this current prediction HTTP request. This id can be used as a reference in logs to identify and track individual prediction requests.
-    prediction_id = str(uuid.uuid4())
+    app.logger.info("Predict endpoint was hit")
+    # img_name = request.args.get('imgName')
+    if 'imgName' in request.args:
+        img_name = request.args['imgName']
+        app.logger.info(f"Received request for image: {img_name}")
 
+    else:
+        return 'Error: imgName parameter is required', 400
+    logger.info(f'Received image name: {img_name}')
+
+    prediction_id = str(uuid.uuid4())
     logger.info(f'prediction: {prediction_id}. start processing')
 
-    # Receives a URL parameter representing the image to download from S3
-    img_name = request.args.get('imgName')
+    original_img_path = Path(f'images/{img_name}')
+    try:
+        download_from_s3(images_bucket, img_name, str(original_img_path))
+    except ClientError as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
 
-    # TODO download img_name from S3, store the local image path in the original_img_path variable.
-    #  The bucket name is provided as an env var BUCKET_NAME.
+    logger.info(f'Prediction: {prediction_id}. Download img completed')
 
-    s3 = boto3.client('s3')
-    original_img_path = Path(f'static/data/{prediction_id}/{img_name}')
-    s3.download_file(images_bucket, img_name, str(original_img_path))
-    logger.info(f'prediction: {prediction_id}/{original_img_path}. Download img completed')
+    try:
+        run(
+            weights='yolov5s.pt',
+            data='data/coco128.yaml',
+            source=str(original_img_path),
+            project='static/data',
+            name=prediction_id,
+            save_txt=True
+        )
+    except Exception as e:
+        logger.error(f'Error during prediction: {e}')
+        return jsonify({"status": "error", "message": str(e)}), 500
 
-    # Predicts the objects in the image
-    run(
-        weights='yolov5s.pt',
-        data='data/coco128.yaml',
-        source=original_img_path,
-        project='static/data',
-        name=prediction_id,
-        save_txt=True
-    )
+    logger.info(f'Prediction: {prediction_id}. done')
 
-    logger.info(f'prediction: {prediction_id}/{original_img_path}. done')
+    predicted_img_path = Path(f'static/data/{prediction_id}/{img_name}')
+    try:
+        upload_to_s3(str(predicted_img_path), f'{prediction_id}/{img_name}')
+    except ClientError as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
 
-    # This is the path for the predicted image with labels
-    # The predicted image typically includes bounding boxes drawn around the detected objects, along with class labels and possibly confidence scores.
-    predicted_img_path = Path(f'static/data/{prediction_id}/{original_img_path}')
+    pred_summary_path = Path(f'static/data/{prediction_id}/labels/{img_name.split(".")[0]}.txt')
+    logger.info(f"Looking for prediction summary at {pred_summary_path}")
 
-    # TODO Uploads the predicted image (predicted_img_path) to S3 (be careful not to override the original image).
-    s3.upload_file(str(predicted_img_path), images_bucket, f'predicted_{img_name}')
-    logger.info(f'prediction: {prediction_id}/{original_img_path}. Upload predicted img completed')
-
-
-
-    # Parse prediction labels and create a summary
-    pred_summary_path = Path(f'static/data/{prediction_id}/labels/{original_img_path.split(".")[0]}.txt')
     if pred_summary_path.exists():
         with open(pred_summary_path) as f:
             labels = f.read().splitlines()
+            if not labels:
+                logger.error(f'Prediction result is empty at {pred_summary_path}')
+                return jsonify({"status": "error", "message": "Prediction result is empty"}), 404
+
             labels = [line.split(' ') for line in labels]
             labels = [{
-                'class': names[int(l[0])],
-                'cx': float(l[1]),
-                'cy': float(l[2]),
-                'width': float(l[3]),
-                'height': float(l[4]),
-            } for l in labels]
+                'class': names[int(label[0])],
+                'cx': float(label[1]),
+                'cy': float(label[2]),
+                'width': float(label[3]),
+                'height': float(label[4]),
+            } for label in labels]
 
-        logger.info(f'prediction: {prediction_id}/{original_img_path}. prediction summary:\n\n{labels}')
+        logger.info(f'Prediction: {prediction_id}. prediction summary:\n\n{labels}')
 
         prediction_summary = {
             'prediction_id': prediction_id,
-            'original_img_path': original_img_path,
-            'predicted_img_path': predicted_img_path,
+            'original_img_path': str(original_img_path),
+            'predicted-img_path': str(predicted_img_path),
             'labels': labels,
             'time': time.time()
         }
 
-        # TODO store the prediction_summary in MongoDB
-        # i run mongodb in docker container
-        # docker run -d -p 27017:27017 --name mongodb mongo
+        try:
+            # Attempt to insert the document
+            insert_result = collection.insert_one(prediction_summary)
 
-        collection.insert_one(prediction_summary)
-        logger.info(f'prediction: {prediction_id}/{original_img_path}. prediction summary saved to MongoDB')
+            # Log the result of the insertion
+            logger.info(f"Inserted ID: {insert_result.inserted_id}")
 
-        return prediction_summary
-    else:
-        return f'prediction: {prediction_id}/{original_img_path}. prediction result not found', 404
+            # Retrieve the inserted document
+            inserted_doc = collection.find_one({'_id': insert_result.inserted_id})
+
+            # Create a new dict without the '_id' field
+            response_summary = {k: v for k, v in inserted_doc.items() if k != '_id'}
+
+            # Add the inserted ID as a string
+            response_summary['mongo_id'] = str(insert_result.inserted_id)
+
+            logger.info(f"Response summary: {response_summary}")
+
+            return jsonify({
+                "status": "success",
+                "message": "Prediction Done Successfully :D",
+                "result_path": response_summary
+            }), 200
+
+        except Exception as e:
+            logger.error(f"Error during MongoDB operation: {str(e)}")
+            return jsonify({
+                "status": "error",
+                "message": f"An error occurred: {str(e)}"
+            }), 500
 
 
 if __name__ == "__main__":
